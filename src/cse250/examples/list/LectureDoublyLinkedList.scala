7eb80ff624d7d4ee4d55299cf435b779536398b8
Andrew Hughes <ahughes6@buffalo.edu>
2020-03-11 11:50:09 -0400

    added(LectureDoublyLinkedList): Doubly-linked list implementation. Note the improvement on tail removal.

---
+++ src/cse250/examples/list/LectureDoublyLinkedList.scala
@@ -0,0 +1,97 @@
+package cse250.examples.list
+
+class LectureDoublyLinkedList[A] extends collection.mutable.Seq[A]
+  with cse250.examples.types.ListADT[A] {
+
+  class DNode[A](var _value: A, var _next: DNode[A], var _prev: DNode[A])
+
+  private var _headNode: DNode[A] = null
+  private var _tailNode: DNode[A] = null
+  private var _numStored: Int = 0
+
+  override def apply(idx: Int): A = {
+    require(0 <= idx && idx < _numStored)
+    var currentNode = _headNode
+    for (_ <- 0 until idx) currentNode = currentNode._next
+    currentNode._value
+  }
+
+  override def update(idx: Int, elem: A): Unit = {
+    require(0 <= idx && idx < _numStored)
+    var currentNode = _headNode
+    for (_ <- 0 until idx) currentNode = currentNode._next
+    currentNode._value = elem
+  }
+
+  override def insert(idx: Int, elem: A): Unit = {
+    require(0 <= idx && idx <= _numStored)
+    // List empty
+    if (_numStored == 0) {
+      _headNode = new DNode(elem, null, null)
+      _tailNode = _headNode
+      _numStored += 1
+    }
+    // List non-empty
+    else {
+      // Index 0 (new head)
+      if (idx == 0) _headNode = new DNode(elem, _headNode, null)
+      // Between two nodes (not head, not tail)
+      else if (idx < _numStored) {
+        var currentNode = _headNode
+        for (_ <- 0 until idx - 1) currentNode = currentNode._next
+        currentNode._next = new DNode(elem, currentNode._next, currentNode)
+      }
+      // Index _numStored (new tail)
+      else {
+        _tailNode._next = new DNode(elem, null, _tailNode)
+        _tailNode = _tailNode._next
+      }
+      _numStored += 1
+    }
+  }
+
+  override def remove(idx: Int): A = {
+    require(0 <= idx && idx < _numStored)
+    // List is 1 item.
+    if (_numStored == 1) {
+      val retval = _headNode._value
+      _headNode = null
+      _tailNode = _headNode
+      _numStored -= 1
+      retval
+    }
+    // List non-empty
+    else {
+      // Index 0 (remove head)
+      if (idx == 0) {
+        val retval = _headNode._value
+        _headNode = _headNode._next
+        _headNode._prev = null
+        _numStored -= 1
+        retval
+      }
+      // Index _numStored-1 (remove tail)
+      else if (idx == _numStored - 1) {
+        val retval = _tailNode._value
+        _tailNode = _tailNode._prev
+        _tailNode._next = null
+        _numStored -= 1
+        retval
+      }
+      // Not removing head or tail.
+      else {
+        var currentNode = _headNode
+        for (_ <- 0 until idx - 1) currentNode = currentNode._next
+        val retval = currentNode._next._value
+        currentNode._next = currentNode._next._next
+        currentNode._next._prev = currentNode // Unsafe code at tail. Why?
+        _numStored -= 1
+        retval
+      }
+    }
+  }
+
+  override def iterator: Iterator[A] = ???
+
+  override def length: Int = _numStored
+}
